520767b6131cb71f3589e7d2023f028a6e3eba24
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-07-29 21:47:52 +0400

    Разбирались со вторым проектом

---
+++ i18n-2/src/components/FormI18n/index.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "./i18n";
+import { Trans, useTranslation } from "react-i18next";
+import { languages } from "../../utils";
+
+const FormI18n = (props) => {
+  const { t, i18n } = useTranslation();
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const date = new Intl.DateTimeFormat(i18n.language, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(new Date());
+
+  const count = 1;
+  const name = "Vasya";
+
+  return (
+    <div>
+      <div>{t("passedMessage")}</div>
+      <div>{t("CurrentLocale", { locale: i18n.language })}</div>
+      <button onClick={() => i18n.changeLanguage(languages.ru)}>
+        {t(languages.ru)}
+      </button>
+      <button onClick={() => i18n.changeLanguage(languages.en)}>
+        {t(languages.en)}
+      </button>
+      <div>
+        {t("TodayKey")} {date}
+      </div>
+      <input
+        value={identifier}
+        placeholder={t("Login:LoginPlaceholder")}
+        onChange={(event) => {
+          setIdentifier(event.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="password"
+        value={password}
+        placeholder={t("Login:Password", "Password word default")}
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          setIsLoading(true);
+          setTimeout(() => setIsLoading(false), 2000);
+        }}
+      >
+        {t("Login:Login")}
+      </button>
+      {isLoading && <div>{t("LoadingMessage")}</div>}
+
+      <hr />
+
+      <Trans i18nKey="message">
+        Hello <strong>{{ name }}</strong>!!!!
+      </Trans>
+
+      <div>
+        {count === 1
+          ? t("countMsg_one", {
+              count: 1,
+            })
+          : t("countMsg_other", {
+              count: count,
+            })}
+      </div>
+    </div>
+  );
+};
+
+export default FormI18n;
