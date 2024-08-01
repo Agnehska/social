d70ad480479e99bcc4b057544c63958fa92d64df
Agnehska <45421349+Agnehska@users.noreply.github.com>
2024-08-01 16:47:23 +0300

    добавила d-n-d

--- front/src/pages/Video.jsx
+++ front/src/pages/Video.jsx
@@ -4,31 +4,78 @@ import { useFetchData } from "../assets/hooks/useFetchData";
 
 export const Video = () => {
   const [cardList, setCardList] = useState([]);
+  const [cardList2, setCardList2] = useState([
+    {id:1, order: 3, text: "Card 1"},
+    {id:2, order: 1, text: "Card 2"}, 
+    {id:3, order: 2, text: "Card 3"},
+    {id:4, order: 4, text: "Card 4"},
+  ]);
+  const [currentCard, setCurrentCard] = useState(null)
   const { res: video, refetch } = useFetchData("GET", "video", null, null);
   useEffect(() => {
-    setCardList(video);
+    let order = 0;
+    setCardList(video.map(el => {
+      order++;
+      return {...el, order: order}
+    }));
+    
   }, [video]);
 
   function dragStartHandler(e, card){
-    console.log('dragStart', card)
+    console.log('dragStart', cardList)
+    setCurrentCard(card)
   }
 
   function dragEndHandler(e){
-    
+    e.target.style.background = 'white'
   }
 
   function dragOverHandler(e){
     e.preventDefault();
+    e.target.style.background = 'lightgray'
   }
 
   function dropHandler(e, card){
     e.preventDefault();
-    console.log('drop', card)
+    setCardList(cardList.map(item => {
+      if (item._id === card._id){
+        return {...item, order: currentCard.order}
+      }
+      if (item._id === currentCard._id){
+        return {...item, order: card.order}
+      }
+      return item
+    }))
+  }
+
+  const sortCards = (a, b) => {
+    if (a.order > b.order){
+      return 1
+    } else {
+      return -1
+    }
   }
 
   return (
     <div className="flex justify-between gap-0.5 gap-y-5 flex-wrap pt-4">
-      {cardList.map((movie) => {
+      {cardList.sort(sortCards).map(card => {
+        return (
+          <div 
+            key={card._id}
+            className="border cursor-grab"
+            grabble
+            onDragStart={e => dragStartHandler(e, card)}
+            onDragLeave={e => dragEndHandler(e)}
+            onDragEnd={e => dragEndHandler(e)}
+            onDragOver={e => dragOverHandler(e)}
+            onDrop={e => dropHandler(e, card)}
+          >
+            <VideoFile movie={card} />
+            {/* jingiujhtr {card._id} */}
+          </div>
+        )
+      })}
+      {/* {cardList.map((movie) => {
         return <VideoFile 
           key={movie._id}
           movie={movie} 
@@ -39,7 +86,7 @@ export const Video = () => {
           onDrop={(e) => dropHandler(e, movie)}
           draggable={true}
         />;
-      })}
+      })} */}
       {/* <div className="rounded-lg shadow-lg bg-white max-w-sm">
           <a href="#!">
               <video width="320" height="240" controls className="w-full rounded-t-lg">
